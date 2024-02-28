33f3b26583827c9e9690b823eca87f175c25c7cb
minah8 <o_uo_o@naver.com>
2024-02-28 16:44:00 +0900

    # Java oop(인터페이스)

---
+++ src/oop/inter/animal/Animal.java
@@ -0,0 +1,12 @@
+package oop.inter.animal;
+
+public interface Animal {
+
+//    int age;
+//    String name;
+
+    // 내가 앞으로 만들 클래스의 기능을 명세
+    void feed(String foodName);
+    boolean sleep();
+
+}
