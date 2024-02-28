46a3f2b2ba5d312935676371f32773ae5f033a6c
minah8 <o_uo_o@naver.com>
2024-02-28 13:17:11 +0900

    # Java oop(static 완료)

---
+++ src/oop/static_/singleton/MainClass.java
@@ -0,0 +1,18 @@
+package oop.static_.singleton;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10000; i++) {
+        Singleton s = Singleton.getInstance();
+        s.method1();
+        s.method2();
+        System.out.println("s = " + s);
+
+        }
+
+
+    }
+
+}
